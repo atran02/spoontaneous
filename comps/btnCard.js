dc8bed64b3a4f30de283d9f5939cb8c0039e1ea4
atran <atran65@my.bcit.ca>
2023-02-19 17:17:18 -0800

    edit comps and styles

--- comps/btnCard.js
+++ comps/btnCard.js
@@ -1,7 +1,6 @@
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function BtnCard(){
-    return <div>
-        
-    </div>
+    return <div></div>
 }
